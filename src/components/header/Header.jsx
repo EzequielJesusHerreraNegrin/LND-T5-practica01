d5e402becb666d1dfed40306df68e93e0b5a9b7f
EzequielJesusHerreraNegrin <usr3763@salesianos-lacuesta.net>
2024-05-05 21:54:44 +0200

    feat(header): Implementación del componente 'Header'

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.modules.css'
+
+function Header(props) {
+
+    return (
+        <div className="header">
+            <h1 className="header__title">Tema 05</h1>
+            <h2 className="header__subtitle">Práctica 01</h2>
+        </div>
+    )
+}
+
+export default Header
