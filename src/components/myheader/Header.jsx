0ed30ea71dcac0d53bd39359e10945b4ada7a62d
EzequielJesusHerreraNegrin <usr3763@salesianos-lacuesta.net>
2024-05-06 19:24:38 +0200

    feat(header): realizado el header de mi aplicación.

---
+++ src/components/myheader/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from './Header.module.css'
+
+const Header = ({ title, subtitle }) => {
+  return (
+    <div className={styles.headerContainer}>
+      <h1 className={styles.headerTitle}>{title}</h1>
+      <h2 className={styles.headerSubtitle}>{subtitle}</h2>
+    </div>
+  )
+}
+
+Header.propTypes = {
+  title: PropTypes.string,
+  subtitle: PropTypes.string,
+}
+
+export default Header
